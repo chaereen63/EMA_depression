dd02fb489266943b33d44f28d672c3cf3ea3034e
Chae Reen Kang <cherish4@g.skku.edu>
2023-08-09 13:36:04 +0900

    하루, 오전/오후로 분류 오전오후 더미변수 생성

--- multi_level.R
+++ multi_level.R
@@ -4,11 +4,19 @@ library(lavaan)
 library(lubridate)
 
 ema <- read.csv("EMA data_2023-1 bk _time_event.csv")
+data <- read.csv("EMA data_bk_TE.csv")
 ema$time %>% strptime(,format = "%Y-%m-%d %H:%M:%S") -> ema$time
+data$time %>% strptime(,format = "%Y-%m-%d %H:%M") -> data$time
 table(ema$STR_jump)
 summary(ema)
 
 ema %>% mutate(days = day(ema$time)) -> ema_d
+data %>% mutate(days = day(data$time), DinY = yday(time), AMPM = ifelse(am(time)==T, 1, 0)) -> dat_
+                
+                
+##하루 스트레스 빈도
+data %>% filter(STR_YN==1)) %>%
+  group_by(ID, DinY) %>% summarise(count=n()) -> da_
 
 model <- 'level: 1
             STR_YN ~ CM_RUM
